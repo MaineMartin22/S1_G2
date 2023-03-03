149d643f5e4b5729e075ae6b8ef95ca2ada109e5
lucaas2120 <lucaas2120@gmail.com>
2023-03-03 12:11:38 -0300

    Agregando archivos Spring

---
+++ AgenciaDeTurismo/src/main/java/com/sprint1/AgenciaDeTurismo/AgenciaDeTurismoApplication.java
@@ -0,0 +1,13 @@
+package com.sprint1.AgenciaDeTurismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaDeTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaDeTurismoApplication.class, args);
+	}
+
+}
---
+++ AgenciaDeTurismo/src/test/java/com/sprint1/AgenciaDeTurismo/AgenciaDeTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint1.AgenciaDeTurismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaDeTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
