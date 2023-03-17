47210b46e9badf0912085b0f58d0bcf5ccf50523
lucaas2120 <lucaas2120@gmail.com>
2023-03-17 15:45:56 -0300

    Factorys hotels

--- AgenciaDeTurismo/src/test/java/com/sprint1/AgenciaDeTurismo/utils/Hotel/BookingResponseDTOFactory.java
+++ AgenciaDeTurismo/src/test/java/com/sprint1/AgenciaDeTurismo/utils/Hotel/BookingResponseDTOFactory.java
@@ -1,21 +1,41 @@
 package com.sprint1.AgenciaDeTurismo.utils.Hotel;
 
-import com.sprint1.AgenciaDeTurismo.DTO.RequestDto.PeopleDto;
 import com.sprint1.AgenciaDeTurismo.DTO.ResponseDto.Hotel.BookingResponseDto;
+import com.sprint1.AgenciaDeTurismo.utils.Data.PeopleDTOFactory;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public class BookingResponseDTOFactory {
-    public static BookingResponseDto bookingResponseDto(){
+    public static BookingResponseDto bookingResponseDtoIguazuDoble(){
         return BookingResponseDto.builder()
-                .dateFrom()
-                .dateTo()
-                .destination()
-                .hotelCode()
-                .peopleAmount()
-                .roomType()
-                .people()
+                .dateFrom(LocalDate.of(2022, 02, 10))
+                .dateTo(LocalDate.of(2022, 03, 20))
+                .destination("Puerto Iguazú")
+                .hotelCode("CH-0002")
+                .peopleAmount(2)
+                .roomType("Doble")
+                .people(
+                        List.of(
+                                PeopleDTOFactory.getPeopleOne(),
+                                PeopleDTOFactory.getPeopleTwo()
+                        )
+                )
+                .build();
+    }
+    public static BookingResponseDto bookingResponseDtoBuenosAiresSinlge(){
+        return BookingResponseDto.builder()
+                .dateFrom(LocalDate.of(2022, 02, 10))
+                .dateTo(LocalDate.of(2022, 02, 20))
+                .destination("Buenos Aires")
+                .hotelCode("HB-0001")
+                .peopleAmount(1)
+                .roomType("Single")
+                .people(
+                        List.of(
+                                PeopleDTOFactory.getPeopleThree()
+                        )
+                )
                 .build();
     }
 }
--- AgenciaDeTurismo/src/test/java/com/sprint1/AgenciaDeTurismo/utils/Hotel/BookingResponseFactory.java
+++ AgenciaDeTurismo/src/test/java/com/sprint1/AgenciaDeTurismo/utils/Hotel/BookingResponseFactory.java
@@ -3,18 +3,51 @@ package com.sprint1.AgenciaDeTurismo.utils.Hotel;
 import com.sprint1.AgenciaDeTurismo.DTO.ResponseDto.Hotel.BookingResponse;
 import com.sprint1.AgenciaDeTurismo.DTO.ResponseDto.Hotel.BookingResponseDto;
 import com.sprint1.AgenciaDeTurismo.DTO.StatusCodeDto;
+import com.sprint1.AgenciaDeTurismo.utils.Data.PeopleDTOFactory;
 import com.sprint1.AgenciaDeTurismo.utils.Data.StatusCodeDTOFactory;
 
 public class BookingResponseFactory {
-    public static BookingResponse bookingResponse(){
 
+public static BookingResponse getReservationHotelIguazuDebit() {
+    return BookingResponse.builder()
+            .userName(PeopleDTOFactory.getPeopleOne().getMail())
+            .totalNeto(63000)
+            .totalIntereses(0)
+            .totalFinal(63000)
+            .booking(BookingResponseDTOFactory.bookingResponseDtoIguazuDoble())
+            .statusCode(StatusCodeDTOFactory.getStatusCode())
+            .build();
+    }
+    public static BookingResponse getReservationHotelBsAsThreeDues() {
         return BookingResponse.builder()
-                .userName()
-                .totalNeto()
-                .totalIntereses()
-                .totalFinal()
-                .booking()
+                .userName(PeopleDTOFactory.getPeopleThree().getMail())
+                .totalNeto(54350)
+                .totalIntereses(2717.5)
+                .totalFinal(57067.5)
+                .booking(BookingResponseDTOFactory.bookingResponseDtoBuenosAiresSinlge())
                 .statusCode(StatusCodeDTOFactory.getStatusCode())
                 .build();
     }
+    public static BookingResponse getReservationHotelBsAsSixDues() {
+        return BookingResponse.builder()
+                .userName(PeopleDTOFactory.getPeopleThree().getMail())
+                .totalNeto(54350)
+                .totalIntereses(5435)
+                .totalFinal(59785)
+                .booking(BookingResponseDTOFactory.bookingResponseDtoBuenosAiresSinlge())
+                .statusCode(StatusCodeDTOFactory.getStatusCode())
+                .build();
+    }
+    public static BookingResponse getReservationHotelBsAsTwelveDues() {
+        return BookingResponse.builder()
+                .userName(PeopleDTOFactory.getPeopleThree().getMail())
+                .totalNeto(54350)
+                .totalIntereses(8152.5)
+                .totalFinal(62502.5)
+                .booking(BookingResponseDTOFactory.bookingResponseDtoBuenosAiresSinlge())
+                .statusCode(StatusCodeDTOFactory.getStatusCode())
+                .build();
+    }
+
+
 }
